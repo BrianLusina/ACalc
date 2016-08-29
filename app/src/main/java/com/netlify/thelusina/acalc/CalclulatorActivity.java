a8253faba77b6fa9c2d946fdcac78f94d779f8a4
Brian Lusina <lusinabrian@gmail.com>
2016-08-29 08:42:41 +0300

    Add project structure

---
+++ app/src/main/java/com/netlify/thelusina/acalc/CalclulatorActivity.java
@@ -0,0 +1,13 @@
+package com.netlify.thelusina.acalc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalclulatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculator_activity);
+    }
+}
