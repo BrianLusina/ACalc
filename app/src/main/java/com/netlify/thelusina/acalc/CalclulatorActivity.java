af669b0d4623ce66903a0f438e6aadb02f9e879c
Brian Lusina <lusinabrian@gmail.com>
2016-08-29 11:21:05 +0300

    initialize output String builder

--- app/src/main/java/com/netlify/thelusina/acalc/CalclulatorActivity.java
+++ app/src/main/java/com/netlify/thelusina/acalc/CalclulatorActivity.java
@@ -18,6 +18,8 @@ public class CalclulatorActivity extends AppCompatActivity implements View.OnCli
         setContentView(R.layout.calculator_activity);
         initViews();
 
+        output = new StringBuilder();
+
         /*enable JavaScript to WebView*/
         display.getSettings().setJavaScriptEnabled(true);
     }
@@ -91,3 +93,78 @@ public class CalclulatorActivity extends AppCompatActivity implements View.OnCli
         display.loadData(builder.toString(), "application/xhtml", "UTF-8");
     }
 }
+
+
+/**
+ *
+ public class CalculatorActivity extends Activity {
+ private WebView mWebView;
+ private StringBuilder mMathString;
+ private ButtonClickListener mClickListener;
+
+@Override
+public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+
+    // Create the math string
+    mMathString = new StringBuilder();
+
+    // Enable javascript for the view
+    mWebView = (WebView) findViewById(R.id.webview);
+    mWebView.getSettings().setJavaScriptEnabled(true);
+
+    // Set the listener for all the buttons
+    mClickListener = new ButtonClickListener();
+    int idList[] = { R.id.button0, R.id.button1, R.id.button2,
+            R.id.button3, R.id.button4, R.id.button5, R.id.button6,
+            R.id.button7, R.id.button8, R.id.button9, R.id.buttonLeftParen,
+            R.id.buttonRightParen, R.id.buttonPlus, R.id.buttonPlus,
+            R.id.buttonMinus, R.id.buttonDivide, R.id.buttonTimes,
+            R.id.buttonDecimal, R.id.buttonBackspace, R.id.buttonClear };
+
+    for(int id : idList) {
+        View v = findViewById(id);
+        v.setOnClickListener(mClickListener);
+    }
+
+}
+
+    private void updateWebView() {
+
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("<html><body>");
+        builder.append("<script type=\"text/javascript\">document.write('");
+        builder.append(mMathString.toString());
+        builder.append("');");
+        builder.append("document.write('<br />=' + eval(\"");
+        builder.append(mMathString.toString());
+        builder.append("\"));</script>");
+        builder.append("</body></html>");
+
+        mWebView.loadData(builder.toString(), "application/xhtml", "UTF-8");
+    }
+
+private class ButtonClickListener implements OnClickListener {
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonBackspace:
+                if(mMathString.length() > 0)
+                    mMathString.deleteCharAt(mMathString.length()-1);
+                break;
+            case R.id.buttonClear:
+                if(mMathString.length() > 0)
+                    mMathString.delete(0, mMathString.length());
+                break;
+            default:
+                mMathString.append(((Button) v).getText());
+        }
+
+        updateWebView();
+    }
+}
+
+* */
