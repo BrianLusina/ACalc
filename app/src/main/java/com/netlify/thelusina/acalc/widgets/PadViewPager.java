30f95b768639f4d3264335cd7ef6a949955bb701
Brian Lusina <lusinabrian@gmail.com>
2016-09-18 21:21:34 +0300

    Add nineoldandroids lib

--- app/src/main/java/com/netlify/thelusina/acalc/widgets/PadViewPager.java
+++ app/src/main/java/com/netlify/thelusina/acalc/widgets/PadViewPager.java
@@ -1,5 +1,13 @@
 package com.netlify.thelusina.acalc.widgets;
 
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.netlify.thelusina.acalc.R;
+
 /**
  * Project: ACalc
  * Package: com.netlify.thelusina.acalc.widgets
@@ -8,5 +16,94 @@ package com.netlify.thelusina.acalc.widgets;
  * Description:
  */
 
-public class PadViewPager {
+public class PadViewPager extends CustomViewPager{
+
+    private final PagerAdapter mStaticPagerAdapter = new PagerAdapter() {
+        @Override
+        public int getCount() {
+            return getChildCount();
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            return getChildAt(position);
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            removeViewAt(position);
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public float getPageWidth(int position) {
+            return position == 1 ? 7.0f / 9.0f : 1.0f;
+        }
+    };
+
+    private final OnPageChangeListener mOnPageChangeListener = new SimpleOnPageChangeListener() {
+        private void recursivelySetEnabled(View view, boolean enabled) {
+            if (view instanceof ViewGroup) {
+                final ViewGroup viewGroup = (ViewGroup) view;
+                for (int childIndex = 0; childIndex < viewGroup.getChildCount(); ++childIndex) {
+                    recursivelySetEnabled(viewGroup.getChildAt(childIndex), enabled);
+                }
+            } else {
+                view.setEnabled(enabled);
+            }
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            if (getAdapter() == mStaticPagerAdapter) {
+                for (int childIndex = 0; childIndex < getChildCount(); ++childIndex) {
+                    // Only enable subviews of the current page.
+                    recursivelySetEnabled(getChildAt(childIndex), childIndex == position);
+                }
+            }
+        }
+    };
+
+    private final PageTransformer mPageTransformer = new PageTransformer() {
+        @Override
+        public void transformPage(View view, float position) {
+            if (position < 0.0f) {
+                // Pin the left page to the left side.
+
+                ViewHelper.setTranslationX(view, getWidth() * -position);
+                ViewHelper.setAlpha(view, Math.max(1.0f + position, 0.0f));
+            } else {
+                // Use the default slide transition when moving to the next page.
+                ViewHelper.setTranslationX(view, 0.0f);
+                ViewHelper.setAlpha(view, 1.0f);
+            }
+        }
+    };
+    public PadViewPager(Context context) {
+        this(context, null);
+    }
+
+    public PadViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        setAdapter(mStaticPagerAdapter);
+        setBackgroundColor(getResources().getColor(android.R.color.black));
+        setOnPageChangeListener(mOnPageChangeListener);
+        setPageMargin(getResources().getDimensionPixelSize(R.dimen.pad_page_margin));
+        setPageTransformer(false, mPageTransformer);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        // Invalidate the adapter's data set since children may have been added during inflation.
+        if (getAdapter() == mStaticPagerAdapter) {
+            mStaticPagerAdapter.notifyDataSetChanged();
+        }
+    }
 }
