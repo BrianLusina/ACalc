f56a129a66596e02f9e1f6f2b6b9a38322b4b307
Brian Lusina <lusinabrian@gmail.com>
2016-09-18 21:16:34 +0300

    Add custom calculator viewpager

---
+++ app/src/main/java/com/netlify/thelusina/acalc/widgets/PadViewPager.java
@@ -0,0 +1,12 @@
+package com.netlify.thelusina.acalc.widgets;
+
+/**
+ * Project: ACalc
+ * Package: com.netlify.thelusina.acalc.widgets
+ * Created by lusinabrian on 18/09/16 at 21:08
+ * <p>
+ * Description:
+ */
+
+public class PadViewPager {
+}
