1211cbe1642f2593a9dc90e679b5e2e27a584e42
Brian Lusina <lusinabrian@gmail.com>
2016-09-19 08:50:53 +0300

    Add initialActivity

---
+++ app/src/main/java/com/netlify/thelusina/acalc/views/CalculatorLol.java
@@ -0,0 +1,11 @@
+package com.netlify.thelusina.acalc.views;
+
+/**
+ * Project: ACalc
+ * Package: com.netlify.thelusina.acalc.views
+ * Created by lusinabrian on 19/09/16 at 08:49
+ * <p>
+ * Description:
+ */
+public class CalculatorLol {
+}
